9ee3160c380f20c9ec6cffcbfa10676588e850aa
Jisoo Choi <jschoi.py@gmail.com>
2022-12-19 00:36:47 +0900

    third commit

--- src/main/java/com/example/firstproject/entity/Article.java
+++ src/main/java/com/example/firstproject/entity/Article.java
@@ -5,21 +5,18 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
 @Getter
-@Entity // DB가 해당 객체를 인식 가능
+@Entity // DB가 해당 객체를 인식 가능 (해당 클래스로 테이블을 만듦)
 public class Article {
 
     @Id // 유일무이한 대표값을 지정
-    @GeneratedValue // 자동생성하기위한 어노테이션 1, 2, 3, ...
+    @GeneratedValue(strategy= GenerationType.IDENTITY) // DB가 id를 자동 생성 어노테이션
     private Long id;
 
     @Column
@@ -28,4 +25,10 @@ public class Article {
     @Column
     private String content;
 
+    public void patch(Article article) {
+        if (article.title != null)
+            this.title = article.title;
+        if (article.content != null)
+            this.content = article.content;
+    }
 }
